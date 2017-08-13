9c0939d9c0380ccbc36190a711c460c4b27eb7c9
Yunpeng Hou <yunpenghou@Yunpengs-MacBook-Pro.local>
2017-08-13 12:07:13 -0400

    Remove sudo from run script; change TODO and README

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 scripts for install openvpn
 
 Reference:
+1. Config openvpn on ubuntu machine
 https://www.digitalocean.com/community/tutorials/how-to-set-up-an-openvpn-server-on-ubuntu-16-04
+
+2. Config to run server at certain time
+https://www.debian-tutorials.com/crontab-tutorial-cron-howto
--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -7,7 +7,7 @@ function stop_vpn_server()
     if [[ $(pgrep openvpn) ]]; then
 
         echo "Stopping vpn server: openvpn..."
-        sudo killall -SIGINT openvpn
+        killall -SIGINT openvpn
 
         # Sleep for 2 seconds and waiting for server to stop
         sleep 2
@@ -35,12 +35,21 @@ function start_vpn_server()
 
     # Start server
     echo "Starting openvpn server..."
-    sudo openvpn ${config_path}
+    openvpn ${config_path} &
     status=$?
     if [ $status != 0 ]; then
         echo "Failed to start openvpn server, exit..."
         exit 1
     fi
+
+    # Check if server is up
+    if [[ $(pgrep openvpn) ]]; then
+        echo "Openvpn server started successfully!"
+    else
+        echo "Failed to start openvpn server, exit..."
+        exit 1
+    fi
+    
 }
 
 # -------------
@@ -49,8 +58,23 @@ function start_vpn_server()
 # Cannot add "" to the path variable, don't know why???
 config_path=~/bin/openvpn_cfg/default/server/openvpn_server1.conf
 
-if [ $# -gt 0 ] && [ $1 == "stop" ]; then
-    stop_vpn_server
+if [ $# -gt 0 ]; then
+    if [ $1 == "stop" ]; then
+        stop_vpn_server
+    else
+        if [ $1 == "help" ]; then
+            echo "Help page:"
+            echo -e "\trun_server.sh -> stop running openvpn server if possible and restart"
+            echo -e "\trun_server.sh stop -> stop running openvpn server"
+            echo -e "\trun_server.sh help -> show this help page"
+            exit 0
+        else
+            echo "Unknown command: $1"
+            echo "Please run <run_server.h help> to see the available commands"
+            exit 1
+        fi
+    fi
+
 else
     start_vpn_server $config_path
 fi
